ee7e393ba1d4539806eaad5afce57cba9303c3da
carbonlytics <stevie@carbonlytics.com>
2019-09-07 10:27:17 -0700

    Added docstring

--- data_analysis.py
+++ data_analysis.py
@@ -1,1 +1,11 @@
 import os
+
+def plot_twitter_sentiment():
+    """This function will fetch, analyze, and plot data from Twitter.
+    
+    Detailed description of the function.
+    """
+    return None
+
+print(plot_twitter_sentiment.__doc__)
+help(plot_twitter_sentiment)
